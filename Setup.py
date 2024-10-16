49a9ad6af5892e0917ee34f6fc9671092ab88e94
juto222 <thomasjulien3101@gmail.com>
2024-10-16 11:24:32 +0200

    Add files via upload

---
+++ Setup.py
@@ -0,0 +1,18 @@
+import sys
+import os
+import main
+
+
+print("Installation des modules pour mon tool:")
+
+
+if sys.platform.startswith("win"):
+    os.system("python -m pip install -r requirements.txt")
+    os.system("python main.py")
+
+elif sys.platform.startswith("linux"):
+    os.system("python3 -m pip3 install -r requirements.txt")
+    os.system("python3 main.py")
+
+
+
