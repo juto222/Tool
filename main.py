3738404a29d44d34721d3cdcca9b48ab8d7be6da
juto222 <thomasjulien3101@gmail.com>
2024-10-22 22:03:54 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,20 +6,24 @@ import requests
 import nmap
 import re
 import ipaddress
+import webbrowser
 
-from Option import GenererIPv6
-from Option import MdpGenerer
-from Option import WebCheck
-from Option import Sousdomaine
-from Option import IPLookup
-from Option import HashFichier
-from Option import HashMDP
-from Option import VerifMDP
-from Option import Conv
+from Options import ScanNetwork
+from Options import GenererIPv6
+from Options import MdpGenerer
+from Options import WebCheck
+from Options import Sousdomaine
+from Options import IPLookup
+from Options import HashFichier
+from Options import HashMDP
+from Options import VerifMDP
+from Options import Conv
+from Options import DDoS
+from Options import Whois
+from Options import UsernameLookup
 
 def afficher_menu():
-    print("""
-          
+    print("""  
                 ░▒▓████████▓▒░  ░▒▓██████▓▒░   ░▒▓██████▓▒░  ░▒▓█▓▒░              
                    ░▒▓█▓▒░     ░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░              
                    ░▒▓█▓▒░     ░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░              
@@ -30,6 +34,7 @@ def afficher_menu():
 
                             https://discord.gg/6WjWn5f6RF  
           
+
 ────────────|[Network] ─────────────────────[Mot de passe]────────────────────────────────[PC]────────────────  
 │                                │                                   │
 │────1  Random IPv6 Generateur   │───3  Générer un mot de passe      │────4  Vérifier si un fichier est un malware
@@ -62,9 +67,14 @@ while True:
 
     if choix == 1:
         try:
-            GenererIPv6.generate_ipv6()
+            GenererIPv6.generate_ipv6() 
         except Exception as e:
             print(f"Erreur lors de la génération d'IPv6 : {e}")
+    elif choix == 2:
+        try:
+            ScanNetwork.scan_network()  
+        except Exception as e:
+            print(f"Erreur lors du scan du réseau : {e}")
     elif choix == 3:
         try:
             MdpGenerer.generer_mot_de_passe()
@@ -72,17 +82,17 @@ while True:
             print(f"Erreur lors de la génération du mot de passe : {e}")
     elif choix == 4:
         try:
-            HashFichier.calculer_hash_fichier()
+            HashFichier.calculer_hash_fichier() 
         except Exception as e:
             print(f"Erreur lors du calcul du hash de fichier : {e}")
     elif choix == 5:
         try:
-            Sousdomaine.sousdomaine()
+            Sousdomaine.sousdomaine() 
         except Exception as e:
             print(f"Erreur lors de la recherche de sous-domaines : {e}")
     elif choix == 6:
         try:
-            HashMDP.hasher_mot_de_passe()
+            HashMDP.hasher_mot_de_passe() 
         except Exception as e:
             print(f"Erreur lors du hachage du mot de passe : {e}")
     elif choix == 7:
@@ -92,12 +102,12 @@ while True:
             print(f"Erreur lors de la vérification du mot de passe : {e}")
     elif choix == 8:
         try:
-            IPLookup.obtenir_infos_ip()
+            IPLookup.obtenir_infos_ip()  
         except Exception as e:
             print(f"Erreur lors de la récupération des informations IP : {e}")
     elif choix == 9:
         try:
-            Conv.convertir()
+            Conv.convertir() 
         except Exception as e:
             print(f"Erreur lors de la conversion : {e}")
     elif choix == 10:
@@ -105,6 +115,24 @@ while True:
             WebCheck.web()
         except Exception as e:
             print(f"Erreur lors de la vérification : {e}")
+    elif choix == 11:
+        url = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+        webbrowser.open_new_tab(url)
+    elif choix == 12:
+        try:
+            DDoS.ddos_attack()
+        except Exception as e:
+            print(f"Une erreur lors de l'attaque est survenue : {e}")
+    elif choix == 13:
+        try:
+            Whois.info_domaine()
+        except Exception as e:
+            print(f"Une erreur est survenue : {e}")
+    elif choix == 14:
+        try:
+            UsernameLookup.username()
+        except Exception as e:
+            print("Une erreur est survenue lors de la recherche : {e}")
     elif choix == 0:
         print("Au revoir!")
         break
