13fb131e6bdbaf88689d60732182d65ad682c355
juto222 <thomasjulien3101@gmail.com>
2024-10-22 21:16:56 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,9 +6,7 @@ import requests
 import nmap
 import re
 import ipaddress
-import webbrowser
 
-from Option import ScanNetwork
 from Option import GenererIPv6
 from Option import MdpGenerer
 from Option import WebCheck
@@ -20,7 +18,8 @@ from Option import VerifMDP
 from Option import Conv
 
 def afficher_menu():
-    print("""  
+    print("""
+          
                 ░▒▓████████▓▒░  ░▒▓██████▓▒░   ░▒▓██████▓▒░  ░▒▓█▓▒░              
                    ░▒▓█▓▒░     ░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░              
                    ░▒▓█▓▒░     ░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░              
@@ -31,28 +30,30 @@ def afficher_menu():
 
                             https://discord.gg/6WjWn5f6RF  
           
-
 ────────────|[Network] ─────────────────────[Mot de passe]────────────────────────────────[PC]────────────────  
 │                                │                                   │
 │────1  Random IPv6 Generateur   │───3  Générer un mot de passe      │────4  Vérifier si un fichier est un malware
 │────2  Scanner Network          │───6  Hacher un mot de passe       │────9  Convertir hexadécimal en décimal
 │────5  Sous domaine             │───7  Vérifier un mot de passe     │────10 Vérifier si un site est en ligne
-│────8  Info sur l'IP            │                                   │────11 Cheat Valorant
+│────12 Attaque DDoS                                                 │────11 Cheat Valorant
 │
+│───────────[OSINT]──────────────
+│ 
+│────8  Info sur l'IP
+│────13 Sous domaine info
+│────14 Username Lookup          
+│          
 │    0- Quitter
     """)
 
 def obtenir_choix():
     try:
         return int(input("""
-        
  ____(user@Tool)
-│__# """))
+│__#  """))
     except ValueError:
         print("Entrée invalide. Veuillez entrer un numéro valide.")
         return -1  # Retourne un choix invalide pour forcer la boucle à redemander l'option
-
-
 
 while True:
     afficher_menu()
@@ -61,14 +62,9 @@ while True:
 
     if choix == 1:
         try:
-            GenererIPv6.generate_ipv6() 
+            GenererIPv6.generate_ipv6()
         except Exception as e:
             print(f"Erreur lors de la génération d'IPv6 : {e}")
-    elif choix == 2:
-        try:
-            ScanNetwork.scan_network()
-        except Exception as e:
-            print(f"Erreur lors du scan du réseau : {e}")
     elif choix == 3:
         try:
             MdpGenerer.generer_mot_de_passe()
@@ -81,12 +77,12 @@ while True:
             print(f"Erreur lors du calcul du hash de fichier : {e}")
     elif choix == 5:
         try:
-            Sousdomaine.sousdomaine()  
+            Sousdomaine.sousdomaine()
         except Exception as e:
             print(f"Erreur lors de la recherche de sous-domaines : {e}")
     elif choix == 6:
         try:
-            HashMDP.hasher_mot_de_passe() 
+            HashMDP.hasher_mot_de_passe()
         except Exception as e:
             print(f"Erreur lors du hachage du mot de passe : {e}")
     elif choix == 7:
@@ -106,12 +102,9 @@ while True:
             print(f"Erreur lors de la conversion : {e}")
     elif choix == 10:
         try:
-            WebCheck.web() 
+            WebCheck.web()
         except Exception as e:
             print(f"Erreur lors de la vérification : {e}")
-    elif choix == 11:
-        url = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
-        webbrowser.open_new_tab(url)
     elif choix == 0:
         print("Au revoir!")
         break
