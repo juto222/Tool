73ee4241ec2061d01193f6af3220844287437685
juto222 <thomasjulien3101@gmail.com>
2024-10-22 18:26:02 +0200

    Update Setup.py

--- Setup.py
+++ Setup.py
@@ -5,12 +5,5 @@ print("Installation des modules pour mon tool:")
 
 
 if sys.platform.startswith("win"):
-    os.system("pip install -r requirements.txt")
-    os.system("python main.py")
-
-elif sys.platform.startswith("linux"):
-    os.system("python3 -m pip3 install -r requirements.txt")
-    os.system("python3 main.py")
-
-
-
+    os.system("pip install dnspython")
+    os.system("pip install nmap")
