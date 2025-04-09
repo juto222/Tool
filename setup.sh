9ab84685f2238be9d6b9abee8fcaee5b007da7bb
juto222 <thomasjulien3101@gmail.com>
2025-04-09 10:23:03 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -12,5 +12,8 @@ python3 -m pip install --upgrade pip
 # Installation des packages
 pip install dnspython python-nmap setuptools pyinstaller auto-py-to-exe urllib3 PyQt5 PyQtWebEngine colorama psutil keyboard requests deep-translator phonenumbers bcrypt beautifulsoup4 pypiwin32 whois cryptography screeninfo GPUtil pycryptodome discord.py discord Pillow browser-cookie3 opencv-python pyautogui
 
+
+echo -e "EN CAS D'ERREUR DE LIBRAIRIE, REINSTALLER LES MANNUELLEMENT JUSQU'À QUE ÇA MARCHE !!!!!"
+
 echo -e "\n\nInstallation finie ! Vous pouvez lancer main.py"
 echo -e 'Pensez à faire "deactivate" à la fin de votre utilisation.'
